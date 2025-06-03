6b27897e2627499eaf6e1f5ac56157f9ddd98ffe
Jens W. Klein <jk@kleinundpartner.at>
2025-06-03 15:22:54 +0200

    chore(deps): upgrade dependencies (#89)
    * chore(deps): upgrade dependencies
    
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/bluedynamics/cdk8s-plone/actions/runs/15406822771
    
    ------
    
    *Automatically created by projen via the "upgrade-main" workflow*
    
    Signed-off-by: github-actions <github-actions@github.com>
    
    * fix eslintrc to ignore imports
    
    * use lowest supported cdk8splus
    
    * use newer jsii
    
    * update locks
    
    * update locks
    
    * chore: self mutation
    
    Signed-off-by: github-actions <github-actions@github.com>
    
    ---------
    
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,14 +1,14 @@
-import { cdk } from 'projen';
+import { cdk, JsonPatch } from 'projen';
 import { NpmAccess } from 'projen/lib/javascript';
 
-const kplus = 'cdk8s-plus-29';
-const constructs = 'constructs@^10.0.0';
+const kplus = 'cdk8s-plus-30';
+const constructs = 'constructs@^10.0.3';
 const project = new cdk.JsiiProject({
   // majorVersion: 1,
   author: 'Jens W. Klein',
   authorAddress: 'jk@kleinundpartner.at',
   defaultReleaseBranch: 'main',
-  jsiiVersion: '~5.5.0',
+  jsiiVersion: '~5.8.0',
   name: '@bluedynamics/cdk8s-plone',
   projenrcTs: true,
   repositoryUrl: 'https://github.com/bluedynamics/cdk8s-plone.git',
@@ -25,7 +25,7 @@ const project = new cdk.JsiiProject({
   devDeps: [
     constructs, // this is ignored by projen
     kplus,
-    'yaml@^2.6.0',
+    'yaml@^2.8.0',
   ],
   publishToPypi: {
     distName: 'cdk8s-plone',
@@ -37,5 +37,12 @@ const project = new cdk.JsiiProject({
   npmProvenance: true,
   npmAccess: NpmAccess.PUBLIC,
 });
+
+
+const eslintJson = project.tryFindObjectFile('.eslintrc.json');
+if (!eslintJson) {
+  throw new Error('.eslintrc.json not found');
+}
+eslintJson.patch(JsonPatch.add('/ignorePatterns/-', 'imports/'));
 
 project.synth();
--- src/plone.ts
+++ src/plone.ts
@@ -1,5 +1,5 @@
 import { Names } from 'cdk8s';
-import * as kplus from 'cdk8s-plus-29';
+import * as kplus from 'cdk8s-plus-30';
 import { Construct } from 'constructs';
 import { PloneDeployment, PloneDeploymentOptions } from './deployment';
 import { IntOrString } from './imports/k8s';
